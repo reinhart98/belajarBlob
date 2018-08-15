9658bd05727d88e8f04a69fcb1ab7f71a849878a
shinogamii <davidreinhart98@gmail.com>
2018-08-15 02:52:13 +0000

    wew

---
+++ tgtest.py
@@ -0,0 +1,7 @@
+from pytel import tg
+
+telegram = tg.Telegram('unix:///tmp/tg.sock') # For Unix Domain Socket
+# telegram = tg.Telegram('tcp://127.0.0.1:4444') # For TCP socket
+
+message = 'Dude! Whats?'
+telegram.send_message('David Reinhart', message)
